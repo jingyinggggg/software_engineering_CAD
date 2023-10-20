12cf04c779c22cbe9d8d8e5f41985e7302349346
jingyinggggg <133077421+jingyinggggg@users.noreply.github.com>
2023-10-20 11:55:03 +0800

    Clinic Page
    - Done retrieve clinic details from database

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from resetPassword import ResetPasswordPage
 from welcome import WelcomePage
 from doctorHomepage import DoctorHomepage
 from profile import ProfilePage
+from medicalRecord import MedicalRecordPage
+from addMedicalRecord import AddMedicalRecordPage
+from viewMedicalRecord import ViewMedicalRecordPage
+from clinic import ClinicPage
+from viewClinic import ViewClinicPage
+from clinicSignUp import ClinicSignUpPage
 
 
 def main(mainPage: Page):
@@ -19,7 +25,13 @@ def main(mainPage: Page):
         path(url="/homepage/:user_id", clear=False, view=Homepage().view),
         path(url="/resetPassword", clear=False, view=ResetPasswordPage().view),
         path(url="/profile/:user_id", clear=False, view=ProfilePage().view),
+        path(url="/medicalRecord/:user_id", clear=False, view=MedicalRecordPage().view),
+        path(url="/addMedicalRecord/:user_id", clear=False, view=AddMedicalRecordPage().view),
+        path(url="/viewMedicalRecord/:medicalRecord_id", clear=False, view=ViewMedicalRecordPage().view),
+        path(url="/clinic/:user_id", clear=False, view=ClinicPage().view),
+        path(url="/viewClinic/:user_id:clinic_id", clear=False, view=ViewClinicPage().view),
         path(url="/login/homepage", clear=False, view=DoctorHomepage().view),
+        path(url="/clinicSignUp", clear=False, view=ClinicSignUpPage().view),
 
     ]
 
@@ -28,4 +40,4 @@ def main(mainPage: Page):
 
 
 if __name__ == "__main__":
-    flet.app(target=main)
+    flet.app(target=main, upload_dir="pic")
