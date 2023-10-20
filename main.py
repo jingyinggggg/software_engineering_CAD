0edcfb613be487749f38582ef421c774a88e6db7
tgy0717 <133035520+tgy0717@users.noreply.github.com>
2023-10-20 12:10:24 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from profile import ProfilePage
 from medicalRecord import MedicalRecordPage
 from addMedicalRecord import AddMedicalRecordPage
 from viewMedicalRecord import ViewMedicalRecordPage
+from clinic import ClinicPage
+from viewClinic import ViewClinicPage
+from clinicSignUp import ClinicSignUpPage
 
 
 def main(mainPage: Page):
@@ -25,7 +28,10 @@ def main(mainPage: Page):
         path(url="/medicalRecord/:user_id", clear=False, view=MedicalRecordPage().view),
         path(url="/addMedicalRecord/:user_id", clear=False, view=AddMedicalRecordPage().view),
         path(url="/viewMedicalRecord/:medicalRecord_id", clear=False, view=ViewMedicalRecordPage().view),
+        path(url="/clinic/:user_id", clear=False, view=ClinicPage().view),
+        path(url="/viewClinic/:user_id:clinic_id", clear=False, view=ViewClinicPage().view),
         path(url="/login/homepage", clear=False, view=DoctorHomepage().view),
+        path(url="/clinicSignUp", clear=False, view=ClinicSignUpPage().view),
 
     ]
 
@@ -34,4 +40,4 @@ def main(mainPage: Page):
 
 
 if __name__ == "__main__":
-    flet.app(target=main)
+    flet.app(target=main, upload_dir="pic")
