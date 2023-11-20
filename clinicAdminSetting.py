e8debb523144453ab75cfab97dbc80fbacd67a68
tgy0717 <133035520+tgy0717@users.noreply.github.com>
2023-11-20 18:34:37 +0800

    Clinic Admin Setting Page
    1. clinicAdminSetting.py included several option such as account,  password & security and log out.
    2. clinicAdminAccountPage.py - display clinic admin information, admin can update the details.
    3. clinicAdminPasswordpage.py - display clinic admin password, admin can change the password.
    4. main.py - update the interface path of clinicAdminSetting.py, clinicAdminAccountPage.py and clinicAdminPasswordpage.py

---
+++ clinicAdminSetting.py
@@ -0,0 +1,204 @@
+from flet import *
+from flet_route import Params, Basket
+import sqlite3
+
+db = sqlite3.connect("cad.db", check_same_thread=False)
+
+
+class ClinicAdminSettingPage:
+    def __init__(self):
+        pass
+
+    def view(self, page: Page, params: Params, basket: Basket):
+        user_id = int(params.user_id)
+
+        page.title = "Call A Doctor"
+        page.window_width = 380
+        page.window_height = 900
+        page.horizontal_alignment = "center"
+        page.vertical_alignment = "center"
+        page.theme_mode = "dark"
+
+        page.fonts = {
+            "RobotoSlab": "https://github.com/google/fonts/raw/main/apache/robotoslab/RobotoSlab%5Bwght%5D.ttf"
+        }
+
+        blue = "#3386C5"
+        grey = "#71839B"
+        lightBlue = "#D0DCEE"
+
+        return View(
+            "/admin/setting/:user_id",
+            controls=[Container(
+                width=350,
+                height=700,
+                bgcolor="#FFFFFF",
+                border_radius=30,
+                alignment=alignment.center,
+                content=Column(
+                    controls=[
+                        Container(
+                            padding=padding.only(top=25, left=10, bottom=10),
+                            content=Row(
+                                controls=[
+                                    Container(
+                                        content=Image(
+                                            src="pic/back.png",
+                                            color="#3386C5",
+                                            width=20,
+                                            height=20
+                                        ),
+                                        on_click=lambda _: page.go(f"/login/adminHomepage/{user_id}")
+                                    ),
+                                    Text(
+                                        value=" Setting",
+                                        color="#3386C5",
+                                        weight=FontWeight.W_600,
+                                        size=18,
+                                    )
+                                ]
+                            ),
+                        ),
+
+                        Container(
+                            width=350,
+                            height=50,
+                            bgcolor=colors.WHITE,
+                            margin=margin.only(top=5,bottom=10),
+                            shadow=BoxShadow(
+                                spread_radius=1,
+                                blur_radius=15,
+                                color=colors.BLUE_GREY_300,
+                                offset=Offset(0, 0),
+                                blur_style=ShadowBlurStyle.NORMAL,
+                            ),
+                            content=Row(
+                                controls=[
+                                    Container(
+                                        margin=margin.only(left=15),
+                                        content=Icon(
+                                            icons.ACCOUNT_BOX,
+                                            size=20,
+                                            color=blue
+                                        )
+                                    ),
+
+                                    Text(
+                                        width=260,
+                                        value="Account",
+                                        size=14,
+                                        color=grey,
+                                        font_family="RobotoSlab",
+                                        weight=FontWeight.W_500
+                                    ),
+
+                                    Container(
+                                        alignment=alignment.center,
+                                        content=Icon(
+                                            icons.ARROW_RIGHT,
+                                            size=22,
+                                            color=grey
+                                        ),
+                                        on_click=lambda _:page.go(f"/admin/account/{user_id}")
+                                    ),
+                                ]
+
+                            )
+                        ),
+
+                        Container(
+                            width=350,
+                            height=50,
+                            bgcolor=colors.WHITE,
+                            margin=margin.only(bottom=10),
+                            shadow=BoxShadow(
+                                spread_radius=1,
+                                blur_radius=15,
+                                color=colors.BLUE_GREY_300,
+                                offset=Offset(0, 0),
+                                blur_style=ShadowBlurStyle.NORMAL,
+                            ),
+                            content=Row(
+                                controls=[
+                                    Container(
+                                        margin=margin.only(left=15),
+                                        content=Icon(
+                                            icons.LOCK,
+                                            size=20,
+                                            color=blue
+                                        )
+                                    ),
+
+                                    Text(
+                                        width=260,
+                                        value="Password & Security",
+                                        size=14,
+                                        color=grey,
+                                        font_family="RobotoSlab",
+                                        weight=FontWeight.W_500
+                                    ),
+
+                                    Container(
+                                        alignment=alignment.center,
+                                        content=Icon(
+                                            icons.ARROW_RIGHT,
+                                            size=22,
+                                            color=grey
+                                        ),
+                                        on_click=lambda _: page.go(f"/admin/password/{user_id}")
+                                    ),
+                                ]
+
+                            )
+                        ),
+
+                        Container(
+                            width=350,
+                            height=50,
+                            bgcolor=colors.WHITE,
+                            # margin=margin.only(top=-10),
+                            shadow=BoxShadow(
+                                spread_radius=1,
+                                blur_radius=15,
+                                color=colors.BLUE_GREY_300,
+                                offset=Offset(0, 0),
+                                blur_style=ShadowBlurStyle.NORMAL,
+                            ),
+                            content=Row(
+                                controls=[
+                                    Container(
+                                        margin=margin.only(left=15),
+                                        content=Icon(
+                                            icons.LOGOUT,
+                                            size=20,
+                                            color=blue
+                                        )
+                                    ),
+
+                                    Text(
+                                        width=260,
+                                        value="Log Out",
+                                        size=14,
+                                        color=grey,
+                                        font_family="RobotoSlab",
+                                        weight=FontWeight.W_500
+                                    ),
+
+                                    Container(
+                                        alignment=alignment.center,
+                                        content=Icon(
+                                            icons.ARROW_RIGHT,
+                                            size=22,
+                                            color=grey
+                                        ),
+                                        on_click=lambda _: page.go(f"/loginUser")
+                                    ),
+                                ]
+
+                            )
+                        )
+
+                    ]
+                ))
+            ]
+        )
--- main.py
+++ main.py
@@ -2,62 +2,122 @@ import flet
 from flet import *
 from flet_route import Routing, path
 
+# Shared interface
+from welcome import WelcomePage
 from login import LoginPage
 from signUp import SignUpPage
-from homepage import Homepage
 from resetPassword import ResetPasswordPage
-from welcome import WelcomePage
-from doctorHomepage import DoctorHomepage
-from profile import ProfilePage
+
+# Patient's interface
+from homepage import Homepage
 from medicalRecord import MedicalRecordPage
 from addMedicalRecord import AddMedicalRecordPage
 from viewMedicalRecord import ViewMedicalRecordPage
+from setting import SettingPage
+from password import PasswordPage
+from accountSetting import AccountSettingPage
 from clinic import ClinicPage
 from viewClinic import ViewClinicPage
-from clinicSignUp import ClinicSignUpPage
-from notification import Notification
+from doctorListClinic import DoctorListBasedOnClinic
+from doctor import DoctorPage
+from viewDoctor import ViewDoctorPage
+from makeAppointment import MakeAppointmentPage
+from patientPrescription import PatientPrescriptionPage
+from viewPrescription import ViewPrescriptionPage
+from healthTips import HealthTipsPage
+from bookingPage import BookingPage
+from viewBooking import ViewBookingPage
+from profile import ProfilePage
+from patientNotification import PatientNotificationPage
+from patientChatViewDoctor import PatientChatViewDoctorPage
+from patientChat import PatientChatPage
+
+# Doctor's interface
+from doctorHomepage import DoctorHomepage
+from doctorNotification import DoctorNotification
 from history import HistoryPage
 from schedule import Schedule
 from appointmentDetail import AppointmentDetail
 from chat import Chat
 from prescription import Prescription
-from addDoctorDetails import AddDoctorDetailsPage
-from doctor import DoctorPage
-from viewDoctor import ViewDoctorPage
-from healthTips import HealthTipsPage
-from makeAppointment import MakeAppointmentPage
-from bookingPage import BookingPage
 
+# Clinic's interface
+from clinicSignUp import ClinicSignUpPage
+from addDoctorDetails import AddDoctorDetailsPage
+from clinicHomepage import ClinicHomepage
+
+# Clinic admin interface
+from clinicAdminHomepage import AdminHomepage
+from clinicAdminPatientRequestList import AdminPatientRequestList
+from clinicAdminUpdateAppointment import ClinicAdminUpdateAppointmentPage
+from clinicAdminUpdateAppointmentDetails import ClinicAdminUpdateAppointmentDetailsPage
+from clinicAdminSetting import ClinicAdminSettingPage
+from clinicAdminAccountPage import  ClinicAdminAccountPage
+from clinicAdminPasswordPage import ClinicAdminPasswordPage
 
 def main(mainPage: Page):
     mainPage.theme_mode = "dark"
 
     app_routes = [
+
+        # Shared interface path
         path(url="/", clear=True, view=WelcomePage().view),
         path(url="/loginUser", clear=False, view=LoginPage().view),
         path(url="/signUp", clear=False, view=SignUpPage().view),
-        path(url="/homepage/:user_id", clear=False, view=Homepage().view),
         path(url="/resetPassword", clear=False, view=ResetPasswordPage().view),
-        path(url="/profile/:user_id", clear=False, view=ProfilePage().view),
+
+
+        # Patient interface path
+        path(url="/homepage/:user_id", clear=False, view=Homepage().view),
+        path(url="/patientNotification/:user_id", clear=False, view=PatientNotificationPage().view),
         path(url="/medicalRecord/:user_id", clear=False, view=MedicalRecordPage().view),
         path(url="/addMedicalRecord/:user_id", clear=False, view=AddMedicalRecordPage().view),
         path(url="/viewMedicalRecord/:medicalRecord_id", clear=False, view=ViewMedicalRecordPage().view),
+        path(url="/setting/:user_id", clear=False, view=SettingPage().view),
+        path(url="/accountSetting/:user_id", clear=False, view=AccountSettingPage().view),
+        path(url="/password/:user_id", clear=False, view=PasswordPage().view),
         path(url="/clinic/:user_id", clear=False, view=ClinicPage().view),
         path(url="/viewClinic/:user_id:clinic_id", clear=False, view=ViewClinicPage().view),
+        path(url="/doctorListBasedOnClinic/:user_id:clinic_id", clear=False, view=DoctorListBasedOnClinic().view),
         path(url="/doctor/:user_id", clear=False, view=DoctorPage().view),
-        path(url="/viewDoctor/:user_id:doctor_id", clear=False, view=ViewDoctorPage().view),
+        path(url="/viewDoctor/:user_id:doctor_id:previous_page", clear=False, view=ViewDoctorPage().view),
+        path(url="/makeAppointment/:user_id:doctor_id:previous_page", clear=False, view=MakeAppointmentPage().view),
+        path(url="/patientPrescription/:user_id", clear=False, view=PatientPrescriptionPage().view),
+        path(url="/viewPrescription/:user_id", clear=False, view=ViewPrescriptionPage().view),
+        # path(url="/viewPrescription/:prescription_id", clear=False, view=PatientPrescriptionPage().view),
         path(url="/healthTips/:user_id", clear=False, view=HealthTipsPage().view),
-        path(url="/makeAppointment/:user_id:doctor_id", clear=True, view=MakeAppointmentPage().view),
-        path(url="/booking/:user_id", clear=True, view=BookingPage().view),
-        path(url="/login/homepage", clear=False, view=DoctorHomepage().view),
+        path(url="/patientChatViewDoctor/:user_id", clear=False, view=PatientChatViewDoctorPage().view),
+        path(url="/patientChat/:user_id:doctor_id", clear=False, view=PatientChatPage().view),
+        path(url="/booking/:user_id", clear=False, view=BookingPage().view),
+        path(url="/viewBooking/:user_id:booking_id", clear=False, view=ViewBookingPage().view),
+        path(url="/profile/:user_id", clear=False, view=ProfilePage().view),
+
+
+        # Doctor interface path
+        path(url="/login/homepage/:user_id", clear=False, view=DoctorHomepage().view),
+        path(url="/doctorNotification/:user_id", clear=False, view=DoctorNotification().view),
+        path(url="/history", clear=False, view=HistoryPage().view),
+        path(url="/schedule", clear=False, view=Schedule().view),
+        path(url="/appointmentDetail/:user_id", clear=False, view=AppointmentDetail().view),
+        path(url="/chat", clear=False, view=Chat().view),
+        path(url="/prescription", clear=False, view=Prescription().view),
+
+
+        # Clinic interface path
         path(url="/clinicSignUp", clear=False, view=ClinicSignUpPage().view),
-        path(url="/notification",clear=False,view=Notification().view),
-        path(url="/history",clear=False,view=HistoryPage().view),
-        path(url="/schedule",clear=False,view=Schedule().view),
-        path(url="/appointmentDetail",clear=False,view=AppointmentDetail().view),
-        path(url="/chat",clear=False,view=Chat().view),
-        path(url="/prescription",clear=False,view=Prescription().view),
-        path(url="/addDoctorDetails", clear=False, view=AddDoctorDetailsPage().view),
+        path(url="/addDoctorDetails/:clinic_id", clear=False, view=AddDoctorDetailsPage().view),
+        path(url="/clinicHomepage/:user_id", clear=False, view=ClinicHomepage().view),
+
+
+        # Clinic admin interface path
+        path(url="/login/adminHomepage/:user_id", clear=False, view=AdminHomepage().view),
+        path(url="/admin/adminPatientRequestList", clear=False, view=AdminPatientRequestList().view),
+        path(url="/admin/clinicAdminUpdateAppointment/:user_id", clear=False, view=ClinicAdminUpdateAppointmentPage().view),
+        path(url="/admin/clinicAdminUpdateAppointmentDetails/:user_id:booking_id", clear=False, view=ClinicAdminUpdateAppointmentDetailsPage().view),
+        path(url="/admin/setting/:user_id", clear=False, view=ClinicAdminSettingPage().view),
+        path(url="/admin/account/:user_id", clear=False, view=ClinicAdminAccountPage().view),
+        path(url="/admin/password/:user_id", clear=False, view=ClinicAdminPasswordPage().view),
+
     ]
     Routing(page=mainPage, app_routes=app_routes)
     mainPage.go(mainPage.route)
